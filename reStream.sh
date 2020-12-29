8db05bdfe1b04c7e47098d97d36136f04b6fe541
Simon Schaefer <simschaefer@ika.rwth-aachen.de>
2020-12-30 00:09:31 +0100

    Adding faster reDump and fixing rotation.

--- reStream.sh
+++ reStream.sh
@@ -80,6 +80,9 @@ if ! ssh_cmd true; then
     exit 1
 fi
 
+# list of ffmpeg filters to apply
+video_filters=""
+
 rm_version="$(ssh_cmd cat /sys/devices/soc0/machine)"
 
 case "$rm_version" in
@@ -94,6 +97,8 @@ case "$rm_version" in
         head_fb0="dd if=/dev/fb0 count=1 bs=$window_bytes 2>/dev/null"
         ;;
     "reMarkable 2.0")
+    	video_filters="$video_filters,transpose=2"
+    
         pixel_format="gray8"
         width=1872
         height=1404
@@ -110,18 +115,15 @@ case "$rm_version" in
         # it is actually the map allocated _after_ the fb0 mmap
         read_address="grep -C1 '/dev/fb0' /proc/$pid/maps | tail -n1 | sed 's/-.*$//'"
         skip_bytes_hex="$(ssh_cmd "$read_address")"
-        skip_bytes="$((0x$skip_bytes_hex + 8))"
-        echo "framebuffer is at 0x$skip_bytes_hex"
+        # Save framebuffer location
+	framebuffer_addresse="$((0x$skip_bytes_hex))"
+	echo "framebuffer is at 0x$framebuffer_addresse"
 
-        # carve the framebuffer out of the process memory
-        page_size=4096
-        window_start_blocks="$((skip_bytes / page_size))"
-        window_offset="$((skip_bytes % page_size))"
-        window_length_blocks="$((window_bytes / page_size + 1))"
+	# carve the framebuffer out of the process memory
+	window_offset=8
 
-        # Using dd with bs=1 is too slow, so we first carve out the pages our desired
-        # bytes are located in, and then we trim the resulting data with what we need.
-        head_fb0="dd if=/proc/$pid/mem bs=$page_size skip=$window_start_blocks count=$window_length_blocks 2>/dev/null | tail -c+$window_offset | head -c $window_bytes"
+	# Use the faster reDump insted of dd, head and tail
+	head_fb0="/home/root/reDump -p $pid -b $framebuffer_addresse -s $window_offset -c $window_bytes"
         ;;
     *)
         echo "Unsupported reMarkable version: $rm_version."
@@ -173,8 +175,6 @@ else
     host_passthrough="cat"
 fi
 
-# list of ffmpeg filters to apply
-video_filters=""
 
 # store extra ffmpeg arguments in $@
 set --
