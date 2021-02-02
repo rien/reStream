0844329d7eb2981baf6bc0f66ed9a2e6eb12fe46
Jakob Frantzvåg Karlsmoen <jakob.karlsmoen@hotmail.no>
2021-02-02 23:10:09 +0100

    Expanded error message to include note about Fedora 33+

--- reStream.sh
+++ reStream.sh
@@ -96,7 +96,7 @@ ssh_cmd() {
 
     if [ -f /etc/os-release ]; then
         . /etc/os-release
-        OS=$NAME
+        OS=$NAME    
         VER=$VERSION_ID
     fi
 
@@ -116,7 +116,7 @@ remarkable_ip() {
 # check if we are able to reach the remarkable
 if ! ssh_cmd true; then
     echo "$remarkable unreachable or you have not set up an ssh key."
-    echo "If you see a 'Permission denied' error, please visit"
+    echo "If you see a 'Permission denied' error or is using Fedora 33+, please visit"
     echo "https://github.com/rien/reStream/#installation for instructions."
     exit 1
 fi
